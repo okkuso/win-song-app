5894f9348fd0b3df4844f9f285f913127fed2962
Shunsuke Okumura <shun.2fb.wb7@gmail.com>
2019-10-18 01:26:09 +0900

    Create new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MediaComponent } from './media/media.component';
+import { TopPageComponent } from './top-page/top-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaComponent,
+    TopPageComponent
   ],
   imports: [
     BrowserModule
