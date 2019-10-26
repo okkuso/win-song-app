3e9bfdb96dd23c286f03a9be5bfa43589f1c8e94
Shunsuke Okumura <shun.2fb.wb7@gmail.com>
2019-10-26 17:13:16 +0900

    Updated Angular 6 -> 8

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterContentInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WinSongService } from '../win-song.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Song } from '../song';
--- src/app/win-song.service.ts
+++ src/app/win-song.service.ts
@@ -9,7 +9,8 @@ import { Song } from './song';
 export class WinSongService {
 
   public songList: Song[];
-  private _url = 'assets/data/songs.json';
+  // private _apiUrl = 'assets/data/songs.json';
+  private _apiUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=q3O2RHPr2Rp2KJs_Rt5XGzMAa3SN6pyFuUk9vzHakWjIx-0yM9YSm-bkBZMpcfkOimxIAk6aA_tjXDtUn_BmmBkbvbOI5f3Qm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnG71DOnfX-4cAtO92GdmSy6MZkxlt2sezGfXx6IqfB8bDEvzQ-qeQRxOkc_-mcFYNo1iM4RU-a3A&lib=M2cIc5_cnOUNLKsLD7MsKPUu1Q3-G6ApM';
 
   constructor( private http: HttpClient ) { }
 
@@ -18,7 +19,7 @@ export class WinSongService {
    *
    */
   getSongList(): void {
-    this.http.get<Song[]>(this._url).subscribe(
+    this.http.get<Song[]>(this._apiUrl).subscribe(
       data => {
         this.songList = data;
       },
